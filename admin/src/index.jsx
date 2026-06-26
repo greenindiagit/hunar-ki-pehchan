0734bc58c6e92bef5209e2f484db579aad87fd87
SARVESH MISHRA <greenindiateam2022@gmail.com>
2026-06-26 10:37:06 +0530

    second

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/">
+   <BrowserRouter basename="/admin">
       <App />
       <ToastContainer
         position="top-right"
